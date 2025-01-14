93a70843f9b716de9a4f8e0610a3fd0293f019e1
YeonwooNuf <jangyw0927@naver.com>
2025-01-14 18:08:01 +0900

    // 한글 인코딩 문제 해결
    게시글 인코딩 문제 해결 (CorsConfig)

--- src/main/java/com/example/workoutbackend/config/CorsConfig.java
+++ src/main/java/com/example/workoutbackend/config/CorsConfig.java
@@ -1,9 +1,14 @@
 package com.example.workoutbackend.config;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.charset.StandardCharsets;
+
 //모든 정책 허용하게 하는 코드
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
@@ -14,5 +19,11 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins("*") // 모든 도메인에서 오는 모든 HTTP 요청을 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
+    }
+
+    // 인코딩 -> UTF-8
+    @Bean
+    public StringHttpMessageConverter stringHttpMessageConverter() {
+        return new StringHttpMessageConverter(StandardCharsets.UTF_8);
     }
 }
